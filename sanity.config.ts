95a4ad3f638e11430ddfab3c6e02537ca1855c10
yuva <yuva.prasath@flyerssoft.com>
2024-09-09 19:53:55 +0530

    Boiler plate setup --main commit--

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@ const config = defineConfig({
   projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
   dataset: `${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
   apiVersion: "2023-7-30",
-  basePath: "/admin",
+  basePath: "/flyerssoft-admin",
   plugins: [structureTool(), visionTool()],
   schema: {
     types: [],
